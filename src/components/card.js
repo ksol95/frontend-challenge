9a83c4adf84d8a52c407d9426b06f3b46ce1a81b
ksol95 <ksol95@ya.ru>
2025-01-15 15:15:54 +0400

    handleLike button

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,17 @@
-export const createCard = (template, catInfo, handleLikeCard) => {
+export const createCard = (template, catInfo) => {
   const card = template.querySelector(".card").cloneNode(true);
   const cardImage = card.querySelector(".card__image");
   const cardLikeButton = card.querySelector(".card__like-button");
 
-  card.setAttribute("catId", catInfo.id);
+  card.setAttribute("cat_id", catInfo.id);
   cardImage.src = catInfo.url;
   cardImage.setAttribute("alt", "Котик");
-  // cardImage.addEventListener("click", () => openImagePopup(cardInfo));
 
-  cardLikeButton.addEventListener("click", handleLikeCard);
-
-  //Определяем ставил ли лайк пользователь
-  // if (cardInfo.likes.some((user) => user._id === userID)) {
-  //   card.setAttribute("liked", true);
-  //   cardLikeButton.classList.add("card__like-button_is-active");
-  // } else {
-  //   card.removeAttribute("liked");
-  // }
+  cardLikeButton.addEventListener("click", () => handleLikeCard(catInfo.id));
   return card;
+};
+
+const handleLikeCard = (id) => {
+  const card = document.querySelector(`[cat_id=${id}]`);
+  console.log(card);
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,13 +4,8 @@ import { createCard } from "../components/card.js";
 const cardTemplate = document.querySelector("#card-template").content;
 const gallary = document.querySelector(".gallary__list");
 
-const renderCat = (cat) =>
-  gallary.append(createCard(cardTemplate, cat, handleLikeCard));
+const renderCat = (cat) => gallary.append(createCard(cardTemplate, cat));
 
 getCards().then((cats) => {
   cats.forEach(renderCat);
 });
-
-const handleLikeCard = () => {
-  console.log("Like");
-};
