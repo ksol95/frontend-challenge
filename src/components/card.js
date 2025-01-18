e7bda9307be934d2d804df7c52ea16b140b4c810
ksol95 <ksol95@ya.ru>
2025-01-18 23:03:13 +0400

    добавил прелоадер к картинкам

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,23 @@ export const createCard = (template, catInfo, handleLikeCard) => {
   const card = template.querySelector(".card").cloneNode(true);
   const cardImage = card.querySelector(".card__image");
   const cardLikeButton = card.querySelector(".card__like-button");
+  const preloader = document.createElement("div");
+  preloader.className = "card__preloader";
+  card.appendChild(preloader);
 
   card.id = catInfo.id;
+  catInfo.like && card.classList.add("like");
+  cardLikeButton.addEventListener("click", () => handleLikeCard(catInfo.id));
+
   cardImage.src = catInfo.url;
   cardImage.setAttribute("alt", "Котик");
-  catInfo.like && card.classList.add("like");
-
-  cardLikeButton.addEventListener("click", () => handleLikeCard(catInfo.id));
+  cardImage.addEventListener("load", () => {
+    preloader.remove();
+  });
+  cardImage.addEventListener("error", () => {
+    console.error(
+      `Ошибка загрузки изображения - "${cardImage.alt}" по адресу ${cardImage.src}`
+    );
+  });
   return card;
 };
